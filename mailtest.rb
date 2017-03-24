8db52dc1bcc35cf44e03d001bb838dddaca28a48
Robert <robert.wlcek@st.ovgu.de>
2017-03-24 15:35:27 +0100

    ...

--- mailtest.rb
+++ mailtest.rb
@@ -11,7 +11,7 @@ Pony.mail to: 'kontakt@infantking.de',
             port:                 '587',
             enable_starttls_auto: true,
             user_name:            'naschwerk@svickova.suhail.uberspace.de',
-            password:             'kuckien0',
+            password:             '',
             authentication:       :login, # :plain, :login, :cram_md5, no auth by default
             domain:               "localhost.localdomain"
           }
